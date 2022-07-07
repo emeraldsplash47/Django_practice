4fe7312de377abc93c8ace331eb34d8d77c015e0
Tahsun <Ehsunrahman@gmail.com>
2022-07-07 17:00:04 +0600

    9th video progress V1

--- Django_practice/blog/views.py
+++ Django_practice/blog/views.py
@@ -4,11 +4,10 @@ from .models import Post
 posts = [
 
     {
-        'author': 'CoryMS',
+        'author': 'CoreyMS',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'August 27, 2018'
-
     },
 
         {
@@ -18,7 +17,6 @@ posts = [
         'date_posted': 'August 28, 2018'
 
     }
-
 ]
 
 def home(request):
