f769d44af9df357f25a65fdd70af17a164a9c584
Tahsun <Ehsunrahman@gmail.com>
2022-07-09 15:14:35 +0600

    progress on 10th video

--- Django_practice/blog/views.py
+++ Django_practice/blog/views.py
@@ -1,31 +1,36 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView)
 
-posts = [
-
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-
-        {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-
-    }
-]
 
 def home(request):
 
     context = {
-        'posts': Post.objects.all()
+        'posts': Post.objects.all() 
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+
